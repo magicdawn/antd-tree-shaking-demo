529bced8d4b2677f87dbe1c8431c665d7ae2288e
Tao <magicdawn@qq.com>
2023-06-14 21:12:47 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import { Button } from 'antd'
+import Button from 'antd/es/button'
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+)
+
+export default App
